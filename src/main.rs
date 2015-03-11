b671799af1a6f2f5bc0c1ec71e6c843c8a88c631
Peter Jacobs <peter@crespyl.net>
2015-03-10 22:33:14 -0400

    Meta-circle

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ extern crate rand;
 extern crate noise;
 extern crate rustbox;
 
+use std::num::Float;
+use std::f32::consts::{PI, PI_2};
 use std::default::Default;
 
 use noise::{perlin2, cell2_value, Brownian2, Seed};
@@ -51,12 +53,19 @@ const UNICODE_CASES: [Cell; 16] = [
     r"███",
 ];
 
-fn corners(x: f32, y: f32, width: f32) -> [[f32; 2]; 4] {
-    let w = width / 2.0;
-    [[x - w, y - w], [x + w, y - w], [x + w, y + w], [x - w, y + w]]
+fn distance_kernel(d: f32, mean: f32, sigma: f32) -> f32 {
+    let g = 1.0 / PI_2.sqrt();
+    let d = (d - mean) / sigma;
+    (g * (-0.5 * d * d) / sigma) + 1.0
+        
 }
 
-fn march(samples: &[f32; 4], threshold: f32, unicode: bool) -> Cell {
+fn corners(x: f32, y: f32, width: f32) -> Vec<[f32; 2]> {
+    let w = width / 2.0;
+    vec![[x - w, y - w], [x + w, y - w], [x + w, y + w], [x - w, y + w]]
+}
+
+fn march(samples: Vec<f32>, threshold: f32, unicode: bool) -> Cell {
     let bits: Vec<usize> = samples.iter().map(|&s| if s > threshold { 1 } else { 0 }).collect();
     let case = bits[0] << 3 | bits[1] << 2 | bits[2] << 1 | bits[3];
     if unicode { UNICODE_CASES[case] } else { CASES[case] }
@@ -73,12 +82,19 @@ fn main() {
     field.add_noise(Box::new(perlin2));
     field.add_noise(Box::new(|seed, &[x, y]| cell2_value(seed, &[x, y]) / 2.0));
     field.add_noise(Box::new(Brownian2::new(perlin2, 5).wavelength(3.0)));
+
+    // field.add_noise(Box::new(|seed, &[x, y]| {
+    //     let d = ((x*x) + (y*y)).sqrt();
+    //     let k = distance_kernel(d, 0.0, 1.0);
+    //     1.0 / k
+    // }));
     
     let mut running = true;
     let mut unicode = false;
     let mut step = 0.1f32;
     let mut threshold = 0.4;
     let (mut startx, mut starty) = (0.0f32, 0.0f32);
+    let (mut cx, mut cy) = (5.0f32, 0.0f32);
 
     while running {
         let (rows, cols) = (rb.height(), rb.width() / 3);
@@ -88,14 +104,20 @@ fn main() {
         for oy in 0..rows {
             for ox in 0..cols {
                 let points = corners(x, y, step);
-                let samples = [field.sample(&points[0]),
-                               field.sample(&points[1]),
-                               field.sample(&points[2]),
-                               field.sample(&points[3])];
+                let samples = vec![field.sample(&points[0]),
+                                   field.sample(&points[1]),
+                                   field.sample(&points[2]),
+                                   field.sample(&points[3])].iter().zip(
+                    points.iter().map(|&[sx, sy]| { 
+                        let (sx, sy) = (sx * 1.5, sy);
+                        let d = ((sx-cx)*(sx-cx) + (sy-cy)*(sy-cy)).sqrt();
+                        let k = 1.0 / distance_kernel(d, 0.0, 1.0);
+                        if k > 0.3 { k*-2.0 } else { 0.0 }
+                    })).map(|(&s, d)| s + d).collect();
 
                 rb.print(ox * 3, oy,
                          rustbox::RB_NORMAL, Color::White, Color::Black, 
-                         march(&samples, threshold, unicode));
+                         march(samples, threshold, unicode));
                 x += step;
             }
             y += step;
@@ -110,6 +132,11 @@ fn main() {
                 Key::Char('s') | Key::Down  => starty += step,
                 Key::Char('a') | Key::Left  => startx -= step,
                 Key::Char('d') | Key::Right => startx += step,
+
+                Key::Char('i') => cy -= step / 2.0,
+                Key::Char('k') => cy += step / 2.0,
+                Key::Char('j') => cx -= step / 2.0,
+                Key::Char('l') => cx += step / 2.0,
 
                 Key::Char('+') if step > 0.01 => step -= 0.001,
                 Key::Char('-') => step += 0.001,
